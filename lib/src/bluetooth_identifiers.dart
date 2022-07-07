4055b900f58ebb2cc87013c9927fc5912d63f77c
Adrian Lemus <16787847+LEMUSADR000@users.noreply.github.com>
2022-07-06 19:12:49 -0500

    added support for byte array and redid some of package documentation.

--- lib/src/bluetooth_identifiers.dart
+++ lib/src/bluetooth_identifiers.dart
@@ -5,6 +5,10 @@ export 'package:bluetooth_identifiers/src/extensions/map_extension.dart';
 part './identifiers/company_identifiers.dart';
 part './identifiers/uuid_service_identifiers.dart';
 
+/// [BluetoothIdentifiers] access point for getting assigned numbers. Abstract
+/// only because there is being no real reason for to instantiate this class
+/// since it simply routes the private global maps.
+///
 abstract class BluetoothIdentifiers {
   static Map<int, String> get companyIdentifiers => _companyIdentifiers;
   static Map<int, UUIDAllocation> get uuidServiceIdentifiers =>
